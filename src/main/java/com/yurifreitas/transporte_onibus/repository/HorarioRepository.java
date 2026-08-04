b5da30f8cb328c5bf508ca89bb4a26692769943c
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-04 17:24:55 -0300

    feat: adiciona um método (findByNumeroLinhaOrderByHorarioAsc) ao HorarioRepository

--- src/main/java/com/yurifreitas/transporte_onibus/repository/HorarioRepository.java
+++ src/main/java/com/yurifreitas/transporte_onibus/repository/HorarioRepository.java
@@ -4,8 +4,12 @@ import com.yurifreitas.transporte_onibus.entity.HorarioEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.LocalTime;
+import java.util.List;
 
 public interface HorarioRepository extends JpaRepository<HorarioEntity, Long> {
 
 	LocalTime findHorario(LocalTime horario);
+
+	// Encontra o número da linha por horario ascendente. (String numeroLinha somente por parâmetro de busca)
+	List<HorarioEntity> findByNumeroLinhaOrderByHorarioAsc(String numeroLinha);
 }
