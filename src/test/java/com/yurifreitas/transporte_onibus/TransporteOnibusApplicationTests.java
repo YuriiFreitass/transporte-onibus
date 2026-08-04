d4c3edc412a74427d88cdae3288eca8531c61526
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-04 08:56:21 -0300

    chore: configura estrutura inicial do projeto

---
+++ src/test/java/com/yurifreitas/transporte_onibus/TransporteOnibusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yurifreitas.transporte_onibus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransporteOnibusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
