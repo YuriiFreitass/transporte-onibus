b91c60c7d12a80cf6e75ed3e5edf12c76dcd4368
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-05 19:01:32 -0300

    feat: implementa serviço de importação das linhas

--- src/main/java/com/yurifreitas/transporte_onibus/client/LinhaParaImportar.java
+++ src/main/java/com/yurifreitas/transporte_onibus/client/LinhaParaImportar.java
@@ -1,4 +1,9 @@
 package com.yurifreitas.transporte_onibus.client;
 
-public class LinhaParaImportar {
+import com.yurifreitas.transporte_onibus.enums.TipoTarifa;
+
+public record LinhaParaImportar(
+		String identificadorPagina,
+		TipoTarifa tarifa
+) {
 }
--- src/main/java/com/yurifreitas/transporte_onibus/service/ImportacaoService.java
+++ src/main/java/com/yurifreitas/transporte_onibus/service/ImportacaoService.java
@@ -1,4 +1,67 @@
 package com.yurifreitas.transporte_onibus.service;
 
+import com.yurifreitas.transporte_onibus.client.EmpresaVitoriaClient;
+import com.yurifreitas.transporte_onibus.client.EmpresaVitoriaParser;
+import com.yurifreitas.transporte_onibus.client.LinhaParaImportar;
+import com.yurifreitas.transporte_onibus.dto.HorarioRequestDto;
+import com.yurifreitas.transporte_onibus.dto.LinhaRequestDto;
+import com.yurifreitas.transporte_onibus.entity.LinhaEntity;
+import com.yurifreitas.transporte_onibus.enums.TipoTarifa;
+import lombok.RequiredArgsConstructor;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
 public class ImportacaoService {
+
+	private final EmpresaVitoriaClient empresaVitoriaClient;
+	private final EmpresaVitoriaParser empresaVitoriaParser;
+	private final LinhaService linhaService;
+	private final HorarioService horarioService;
+
+	@Transactional
+	public void importar(
+			String identificadorPagina,
+			TipoTarifa tarifa
+	) {
+		Document document =
+				empresaVitoriaClient.buscarPagina(identificadorPagina);
+
+		LinhaRequestDto linhaRequest =
+				empresaVitoriaParser.extrairLinha(document, tarifa);
+
+		LinhaEntity linha =
+				linhaService.salvarOuBuscarExistente(linhaRequest);
+
+		List<HorarioRequestDto> horarios =
+				empresaVitoriaParser.extrairHorarios(
+						document,
+						linhaRequest.numeroLinha()
+				);
+
+		horarioService.substituirHorarios(
+				linha,
+				horarios
+		);
+	}
+	public int importarCatalogo() {
+		Document catalogo =
+				empresaVitoriaClient.buscarCatalogo();
+
+		List<LinhaParaImportar> linhas =
+				empresaVitoriaParser.extrairCatalogo(catalogo);
+
+		for (LinhaParaImportar linha : linhas) {
+			importar(
+					linha.identificadorPagina(),
+					linha.tarifa()
+			);
+		}
+
+		return linhas.size();
+	}
 }
