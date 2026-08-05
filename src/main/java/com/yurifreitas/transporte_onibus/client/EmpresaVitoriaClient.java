f819783b986ad6fb118dd6cffe264f6350042a31
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-04 21:24:51 -0300

    feat: implementa integração inicial com o site da Empresa Vitória

--- src/main/java/com/yurifreitas/transporte_onibus/client/EmpresaVitoriaClient.java
+++ src/main/java/com/yurifreitas/transporte_onibus/client/EmpresaVitoriaClient.java
@@ -1,4 +1,44 @@
 package com.yurifreitas.transporte_onibus.client;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
 public class EmpresaVitoriaClient {
+
+	private static final String BASE_URL =
+			"https://www.evitoria.com.br/linhas-e-horarios/";
+
+	public Document buscarPagina(String identificadorPagina) {
+
+		String url = BASE_URL + identificadorPagina;
+
+		try {
+			return Jsoup.connect(url)
+					.userAgent(
+							"Mozilla/5.0 (Windows NT 10.0; Win64; x64) " +
+									"AppleWebKit/537.36 (KHTML, like Gecko) " +
+									"Chrome/151.0.0.0 Safari/537.36"
+					)
+					.header(
+							"Accept",
+							"text/html,application/xhtml+xml,application/xml;q=0.9," +
+									"image/avif,image/webp,image/apng,*/*;q=0.8"
+					)
+					.header("Accept-Language", "pt-BR,pt;q=0.9,en;q=0.8")
+					.referrer("https://www.google.com/")
+					.timeout(15_000)
+					.followRedirects(true)
+					.get();
+
+		} catch (IOException exception) {
+			throw new IllegalStateException(
+					"Não foi possível acessar a página da Empresa Vitória",
+					exception
+			);
+		}
+	}
 }
--- src/main/java/com/yurifreitas/transporte_onibus/controller/ImportacaoController.java
+++ src/main/java/com/yurifreitas/transporte_onibus/controller/ImportacaoController.java
@@ -1,4 +1,29 @@
 package com.yurifreitas.transporte_onibus.controller;
 
+import com.yurifreitas.transporte_onibus.client.EmpresaVitoriaClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import org.jsoup.nodes.Document;
+
+
+@RestController
+@RequestMapping("/v1/importacoes")
+@RequiredArgsConstructor
 public class ImportacaoController {
+
+	private final EmpresaVitoriaClient empresaVitoriaClient;
+
+	@GetMapping("/teste/{identificadorPagina}")
+	public String testarConexao(
+			@PathVariable String identificadorPagina
+	) {
+		Document document = empresaVitoriaClient.buscarPagina(identificadorPagina);
+
+		return document.title();
+	}
 }
