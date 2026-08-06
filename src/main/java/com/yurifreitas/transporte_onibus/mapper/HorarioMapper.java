3e105d5ae78043d3c7ef2237e41221f47c6c8c6d
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-05 21:07:40 -0300

    fix: corrige mapeamento do número da linha no HorarioMapper

--- src/main/java/com/yurifreitas/transporte_onibus/mapper/HorarioMapper.java
+++ src/main/java/com/yurifreitas/transporte_onibus/mapper/HorarioMapper.java
@@ -4,10 +4,12 @@ import com.yurifreitas.transporte_onibus.dto.HorarioRequestDto;
 import com.yurifreitas.transporte_onibus.dto.HorarioResponseDto;
 import com.yurifreitas.transporte_onibus.entity.HorarioEntity;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface HorarioMapper {
 	HorarioEntity toEntity(HorarioRequestDto request);
 
+	@Mapping(target = "numeroLinha", source = "linha.numeroLinha")
 	HorarioResponseDto toResponseDto(HorarioEntity horarioEntity);
 }
