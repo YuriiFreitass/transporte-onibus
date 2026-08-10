4ac16897f91708ab437a945d6e9b56b4ff647d15
YuriiFreitass <yurifreitas1501@gmail.com>
2026-08-10 18:20:47 -0300

    feat: adiciona CORS em LinhaController

--- src/main/java/com/yurifreitas/transporte_onibus/controller/LinhaController.java
+++ src/main/java/com/yurifreitas/transporte_onibus/controller/LinhaController.java
@@ -9,6 +9,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/v1/linhas")
